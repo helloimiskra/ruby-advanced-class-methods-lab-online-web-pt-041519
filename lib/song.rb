f996b9b727bd92a7f985455db8be1a54fb542014
Iskra B <ibatistapoblete@gmail.com>
2019-05-20 04:45:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,6 +49,12 @@ class Song
     file = filename.split(' - ')
     artist_name = file(0)
     song_name = file(1)[/(.mp3)/] = ''
+
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+    
     #file = filename.split(' - ')
     #file.map {|item| item }
     #chomp(/(.mp3)/)
