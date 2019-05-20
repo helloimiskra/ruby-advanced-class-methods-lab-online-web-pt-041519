112839e3c60507bcca65e8ceaa59e875046672a8
Iskra B <ibatistapoblete@gmail.com>
2019-05-20 02:04:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,22 +1,33 @@
 class Song
-  attr_accessor :name, :artist_name.
-    @@all = []
+  attr_accessor :name, :artist_name
+  @@all = []
 
   def self.all
     @@all
-  end
-
-  def self.create(song)
-    song = self.new
-    song.name = name
-    @@all << song
   end
 
   def save
     self.class.all << self
   end
 
-  def self.destroy_all
-    @@all.clear
+  def self.create
+    song = self.new
+    @@all << song
+    return song
   end
+  # def self.new_by_name(string)
+  #   @@all.map do |song|
+  #     if song == string
+  #       song
+  #     end
+  #   end
+  #end
+
+
+
+
+  def self.destroy_all
+     self.all.clear
+  end
+
 end
